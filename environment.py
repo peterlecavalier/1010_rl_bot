11d6a0443c3c5144f5b1620788e45194f4db5444
Peter LeCavalier <pele6150@colorado.edu>
2022-05-10 15:59:04 -0600

    Completely done with the environment

--- environment.py
+++ environment.py
@@ -66,7 +66,7 @@ pieces_specs = {
 
 
 class game_1010(Env):
-    def __init__(self):
+    def __init__(self, seed=None):
         super(game_1010, self).__init__()
         
         # Grid observation space
@@ -91,16 +91,23 @@ class game_1010(Env):
         
         # pieces available
         self.pieces = [0, 0, 0]
+        self.pieces_colors = [None, None, None]
         
         # Start the game off with 0 points
         self.points = 0
+        
+        # Set up rng
+        self.rng, self.rng_seed = gym.utils.seeding.np_random(seed)
     
     def pick_new_pieces(self, choices=[None, None, None]):
         for i in range(3):
             if choices[i] is None:
-                self.pieces[i] = np.random.randint(1, 20)
+                #self.pieces[i] = np.random.randint(1, 20)
+                self.pieces[i] = self.rng.randint(1, 20)
             else:
                 self.pieces[i] = choices[i]
+            #self.pieces_colors[i] = np.random.randint(256, size=3)
+            self.pieces_colors[i] = self.rng.randint(256, size=3)
         
     
     def reset(self):
@@ -112,17 +119,18 @@ class game_1010(Env):
         
         # Reset grid to be zero everywhere
         self.grid = np.zeros_like(self.grid)
+        self.color_grid = np.zeros_like(self.color_grid)
         
-    def expand_arr(self, arr, mult=2, target_shape=(10,10)):
-        new_arr = np.zeros_like(arr, shape=(arr.shape[0] * mult, arr.shape[1]))
+    def expand_arr(self, arr, mult=2, target_shape=(10,10, 3)):
+        new_arr = np.zeros_like(arr, shape=(arr.shape[0] * mult, arr.shape[1], 3))
         for idx, y in enumerate(arr):
             for m in range(mult):
                 new_arr[idx * mult + m] = y
         
-        newer_arr = np.zeros_like(new_arr, shape=(new_arr.shape[0], new_arr.shape[1] * mult))
-        for idx, x in enumerate(new_arr.T):
+        newer_arr = np.zeros_like(new_arr, shape=(new_arr.shape[0], new_arr.shape[1] * mult, 3))
+        for idx, x in enumerate(np.transpose(new_arr, axes=(1, 0, 2))):
             for m in range(mult):
-                newer_arr[:, idx * mult + m] = x.T
+                newer_arr[:, idx * mult + m] = x
             
         final_arr = np.zeros_like(newer_arr, shape=target_shape)
         
@@ -132,30 +140,46 @@ class game_1010(Env):
         final_arr[start_y:start_y+newer_arr.shape[0], start_x:start_x+newer_arr.shape[1]] = newer_arr
         
         return final_arr
+    
+    def colorize_piece(self, piece_idx):
+        # Generate a random color for the piece and apply it to the array
+        copy_arr = np.copy(pieces_specs[self.pieces[piece_idx]]["arr"])
+        copy_arr = np.stack((copy_arr, copy_arr, copy_arr), axis=-1)
+
+        rgb = self.pieces_colors[piece_idx]
+        copy_arr = np.multiply(copy_arr, rgb)
+        
+        return copy_arr
         
     def render(self, mode ="human"):
         assert mode in ["human", "rgb_array"], "Invalid mode, must be either \"human\" or \"rgb_array\""
         
-        expand_grid = self.expand_arr(np.copy(self.grid), 
+        expand_grid = self.expand_arr(np.copy(self.color_grid), 
                                       mult=3, 
                                       target_shape=(self.grid_shape[0] * 3,
-                                                    self.grid_shape[1] * 3))
+                                                    self.grid_shape[1] * 3, 3))
         exp_pieces = []
-        for i in self.pieces:
+        for idx, i in enumerate(self.pieces):
             if i == 0:
-                exp_pieces.append(np.zeros((10, 10), dtype=int))
+                exp_pieces.append(np.zeros((10, 10, 3), dtype=int))
             else:
-                exp_pieces.append(self.expand_arr(np.copy(pieces_specs[i]["arr"])))
+                exp_pieces.append(self.expand_arr(self.colorize_piece(idx)))
             
         pieces_arr = np.concatenate(exp_pieces, axis=1)
         
         final_show = np.concatenate((expand_grid, 
-                                     np.ones((5, expand_grid.shape[1])) * 0.5,
-                                     pieces_arr))
+                                     np.ones((5, expand_grid.shape[1], 3)) * 255,
+                                     pieces_arr)).astype(np.uint8)
+        
+        final_show = cv2.resize(final_show, (final_show.shape[1] * 20,
+                                             final_show.shape[0] * 20),
+                                interpolation=cv2.INTER_NEAREST)
+        final_show = cv2.putText(final_show, f"Points: {self.points}",
+                                 (50, 660), font, 2,
+                                 (0, 0, 0), 2, cv2.LINE_AA)
         if mode == "human":
             cv2.namedWindow("1010!", cv2.WINDOW_NORMAL)
             cv2.imshow("1010!", final_show)
-            cv2.resizeWindow('1010!', final_show.shape[1] * 20,final_show.shape[0] * 20)
             cv2.waitKey(0)
         
         elif mode == "rgb_array":
@@ -169,7 +193,6 @@ class game_1010(Env):
         # 1) we lose the game
         # 2) the agent attempts to use an empty piece
         # 3) the agent attempts to place a piece in an invalid spot
-        done = False
         
         # The action must be in the bounds of the action space first
         assert self.action_space.contains(action), "Invalid Action"
@@ -195,6 +218,8 @@ class game_1010(Env):
         Returns:
             done: Boolean - whether the action ends the game or not
             grid (optional): ndarray - returns the grid with piece added
+            color_grid (optional): ndarray - returns the color grid with piece added
+            reward (optional): int - reward from the move
         '''
         # Get which piece is being added to the grid
         piece_idx = int(np.floor(action/100))
@@ -202,7 +227,7 @@ class game_1010(Env):
         # Check if trying to act on a null piece, return if so
         if self.pieces[piece_idx] == 0:
             if ret_grid:
-                return True, self.grid, 0
+                return True, self.grid, self.color_grid, 0
             else:
                 return True
         
@@ -222,24 +247,30 @@ class game_1010(Env):
         if (grid_pos_y + piece_shape[0]) > self.grid_shape[0] or\
         (grid_pos_x + piece_shape[1]) > self.grid_shape[1]:
             if ret_grid:
-                return True, self.grid, 0
+                return True, self.grid, self.color_grid, 0
             else:
                 return True
         
+        
+        
+        # Superimpose the piece onto the grid
+        grid_manip = np.copy(self.grid)
+        grid_manip[grid_pos_y:grid_pos_y+piece_shape[0], 
+                   grid_pos_x:grid_pos_x+piece_shape[1]] += piece_arr
+        
         # Check if there are any overlaps, return if so
-        if np.max(self.grid[grid_pos_y:grid_pos_y+piece_shape[0], 
-                   grid_pos_x:grid_pos_x+piece_shape[1]]) > 0:
+        if np.max(grid_manip) > 1:
             if ret_grid:
-                return True, self.grid, 0
+                return True, self.grid, self.color_grid, 0
             else:
                 return True
         elif not ret_grid:
             return False
         
-        # Superimpose the piece onto the grid and return
-        grid_manip = np.copy(self.grid)
-        grid_manip[grid_pos_y:grid_pos_y+piece_shape[0], 
-                   grid_pos_x:grid_pos_x+piece_shape[1]] += piece_arr
+        # Add the piece to the color grid
+        color_grid_manip = np.copy(self.color_grid)
+        color_grid_manip[grid_pos_y:grid_pos_y+piece_shape[0], 
+                   grid_pos_x:grid_pos_x+piece_shape[1]] += self.colorize_piece(piece_idx)
         
         # Set that piece index to 0
         self.pieces[piece_idx] = 0
@@ -247,11 +278,11 @@ class game_1010(Env):
         # Reward for placing the piece
         reward = np.sum(piece_arr)
         
-        return False, grid_manip, reward
+        return False, grid_manip, color_grid_manip, reward
     
     
     def execute_action(self, action):
-        done, grid_manip, reward = self.check_valid_move(action)
+        done, grid_manip, color_grid_manip, reward = self.check_valid_move(action)
         
         if done:
             return True, reward
@@ -271,12 +302,15 @@ class game_1010(Env):
         self.points += reward
         
         self.grid = grid_manip
+        self.color_grid = color_grid_manip
         
         # Actually clear the rows from the grid
         for row in rows:
             self.grid[row] = np.zeros_like(self.grid[row])
+            self.color_grid[row] = np.zeros_like(self.color_grid[row])
         for col in cols:
             self.grid[:, col] = np.zeros_like(self.grid[:, col])
+            self.color_grid[:, col] = np.zeros_like(self.color_grid[:, col])
         
         # Reset the pieces if we just ran out
         if np.sum(self.pieces) == 0:
@@ -288,15 +322,14 @@ class game_1010(Env):
             if not done:
                 break
         
-        if done == True:
-            print("done is true")
-        
         # Returns done as false if there are remaining moves available
         return done, reward
     
 
-if __name__ == "__main__":
-#    from IPython import display
+if __name__ == "__main__":   
+    # If you want to test on a specific seed
+    # env = game_1010(seed=0)
+    # env.action_space.seed(0)
     
     env = game_1010()
     obs = env.reset()
@@ -309,7 +342,7 @@ if __name__ == "__main__":
         while done == True and reward == 0:
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
-            print(action)
+        print(action)
         
         # Render the game
         env.render()
@@ -317,5 +350,5 @@ if __name__ == "__main__":
         if done == True:
             break
     print("HERE")
-    env.render()
+    # env.render()
     env.close()
