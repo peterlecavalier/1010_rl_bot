297715e206dbf0d806d0e95c48e2f6923988234c
Peter LeCavalier <pele6150@colorado.edu>
2022-05-26 12:36:07 -0600

    Added more neurons to the hidden layers

--- train_v3.py
+++ train_v3.py
@@ -10,6 +10,8 @@ from ray.rllib.agents import ppo
 from environment import game_1010_v1
 from ray.rllib.examples.models.action_mask_model import ActionMaskModel
 from datetime import datetime
+import matplotlib.pyplot as plt
+
 
 TRAINING_ITER_STOP = 100000
 EPISODE_LEN_MEAN = 1000000
@@ -20,6 +22,8 @@ ray.init()
 # main part: configure the ActionMaskEnv and ActionMaskModel
 config = {
     "env": game_1010_v1,
+    'gamma': 0.99, 
+    'lr': 5e-05,
     "env_config": {
         "action_space": Discrete(300),
         "observation_space": Box(0, 20, (103,), dtype=int),
@@ -29,11 +33,13 @@ config = {
         "custom_model": ActionMaskModel,
         # keep masking enabled
         "custom_model_config": {"no_masking": False},
+        "fcnet_hiddens": [512, 512, 512],
+        #"fcnet_activation": "relu",
     },
     "num_gpus": 0,
     "num_workers": 4,
     # max steps in any one episode is 1000000
-    "horizon": EPISODE_LEN_MEAN
+    "horizon": EPISODE_LEN_MEAN,
 }
 # config["tf_session_args"]["intra_op_parallelism_threads"] = 64
 # config["tf_session_args"]["inter_op_parallelism_threads"] = 64
@@ -56,12 +62,18 @@ ppo_config = ppo.DEFAULT_CONFIG.copy()
 ppo_config.update(config)
 trainer = ppo.PPOTrainer(config=ppo_config, env=game_1010_v1)
 
+policy = trainer.get_policy()
+policy.model.internal_model.base_model.summary()
+
 time = str(datetime.now())
 time = time[:10] + "_" + time[11:-7]
 time = time.replace(":", "_")
 
 f = open(f"./training_output_{time}.txt", "w")
 f.close()
+
+total_lengths = []
+total_rewards = []
 
 # run manual training loop and print results after each iteration
 for idx in range(TRAINING_ITER_STOP):
@@ -71,14 +83,29 @@ for idx in range(TRAINING_ITER_STOP):
     lengths = results["hist_stats"]['episode_lengths']
     print(f"Length min/max/mean = {min(lengths)}/{max(lengths)}/{results['episode_len_mean']}")
 
+    total_lengths.append(results['episode_len_mean'])
+    total_rewards.append(results['episode_reward_mean'])
+
     f = open(f"./training_output_{time}.txt", "a")
     f.write(f"-----EPISODE {idx}-----\n")
     f.write(f"Reward min/max/mean = {results['episode_reward_min']}/{results['episode_reward_max']}/{results['episode_reward_mean']}\n")
     f.write(f"Length min/max/mean = {min(lengths)}/{max(lengths)}/{results['episode_len_mean']}")
     f.close()
+
+    # Save the checkpoint every 10 iterations
     if idx % 10 == 0:
         trainer.save(f"./checkpoints/{time}")
 
+    # Create a plot of rewards and lengths
+    fig, axs = plt.subplots(2, 1, sharey=False, sharex=True)
+    axs[0].plot(total_rewards)
+    axs[1].plot(total_lengths)
+    axs[0].set_ylabel('Average episode reward')
+    axs[1].set_ylabel('Average episode length')
+    axs[1].set_xlabel('Iteration #')
+
+    # Save the plot
+    plt.savefig(f"./checkpoints/{time}/summary_plots.png", bbox_inches='tight')
 
     # stop training if the target train steps or reward are reached
     if (
